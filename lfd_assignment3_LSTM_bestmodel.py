fe9d7708d3ca6d804218ddb0346a3cdffb1a557b
vivienterhell <131256039+vivienterhell@users.noreply.github.com>
2023-10-15 21:08:04 +0200

    Add files via upload

---
+++ lfd_assignment3_LSTM_bestmodel.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+
+'''This Python script is designed for training and evaluating a text classification model
+using deep learning techniques. It includes functionality for data preprocessing, model
+architecture definition, training, and evaluation. Users can customize the behavior of
+the script using command-line arguments.'''
+
+import random as python_random
+import json
+import argparse
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+from keras.initializers import Constant
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import TextVectorization
+import tensorflow as tf
+
+# Make reproducible as much as possible
+np.random.seed(1234)
+tf.random.set_seed(1234)
+python_random.seed(1234)
+
+    
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--train_file", default='train.txt', type=str,
+                        help="Input file to learn from (default train.txt)")
+    parser.add_argument("-d", "--dev_file", type=str, default='dev.txt',
+                        help="Separate dev set to read in (default dev.txt)")
+    parser.add_argument("-t", "--test_file", type=str,
+                        help="If added, use trained model to predict on test set")
+    parser.add_argument("-e", "--embeddings", default='glove_reviews.json', type=str,
+                        help="Embedding file we are using (default glove_reviews.json)")
+    parser.add_argument("--learning_rate", type=float, default=0.01,
+                        help="Learning rate for the optimizer")
+    parser.add_argument("--loss_function", type=str, default='categorical_crossentropy',
+                        help="Loss function for model training")
+    parser.add_argument("--verbose", type=int, default=2,
+                        help="Verbosity level (0: silent, 1: progress bar, 2: one line per epoch)")
+    parser.add_argument("--batch_size", type=int, default=16,
+                        help="Batch size for training")
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="Number of training epochs")
+    args = parser.parse_args()
+    return args
+
+def read_corpus(corpus_file):
+    '''Read in review data set and returns docs and labels'''
+    documents = []
+    labels = []
+    with open(corpus_file, encoding='utf-8') as f:
+        for line in f:
+            tokens = line.strip()
+            documents.append(" ".join(tokens.split()[3:]).strip())
+            # 6-class problem: books, camera, dvd, health, music, software
+            labels.append(tokens.split()[0])
+    return documents, labels
+
+def read_embeddings(embeddings_file):
+    '''Read in word embeddings from file and save as numpy array'''
+    try:
+        embeddings = json.load(open(embeddings_file, 'r'))
+        return {word: np.array(embeddings[word]) for word in embeddings}
+    except FileNotFoundError:
+        print(f"Error: Embeddings file '{embeddings_file}' not found.")
+        exit(1)  # Exit the script with an error code
+    except json.JSONDecodeError:
+        print(f"Error: Failed to decode JSON in '{embeddings_file}'. Check file format.")
+        exit(1)  # Exit the script with an error code
+
+def get_emb_matrix(voc, emb):
+    '''Get embedding matrix given vocab and the embeddings'''
+    num_tokens = len(voc) + 2
+    word_index = dict(zip(voc, range(len(voc))))
+    # Bit hacky, get embedding dimension from the word "the"
+    embedding_dim = len(emb["the"])
+    # Prepare embedding matrix to the correct size
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = emb.get(word)
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    # Final matrix with pretrained embeddings that we can feed to embedding layer
+    return embedding_matrix
+
+def create_model(Y_train, emb_matrix, args):
+    '''Create the Keras model to use'''
+    learning_rate = args.learning_rate
+    loss_function = args.loss_function
+    optim = SGD(learning_rate=learning_rate)
+    # Take embedding dim and size from emb_matrix
+    embedding_dim = len(emb_matrix[0])
+    num_tokens = len(emb_matrix)
+    num_labels = len(set(Y_train))
+    # Now build the model
+    model = Sequential()
+    model.add(Embedding(num_tokens, embedding_dim, embeddings_initializer=Constant(emb_matrix),trainable=False))
+    # Here you should add LSTM layers (and potentially dropout)
+    model.add(LSTM(
+        units = 32,
+        activation="tanh",
+        recurrent_activation="sigmoid",
+        use_bias=True,
+        kernel_initializer="glorot_uniform",
+        recurrent_initializer="orthogonal",
+        bias_initializer="zeros",
+        unit_forget_bias=True,
+        kernel_regularizer=None,
+        recurrent_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        recurrent_constraint=None,
+        bias_constraint=None,
+        dropout=0.0,
+        recurrent_dropout=0.0,
+        return_sequences=False,
+        return_state=False,
+        go_backwards=False,
+        stateful=False,
+        time_major=False,
+        unroll=False,
+    )) 
+    #Extra dense layer
+    model.add(Dense(units=32, activation="relu"))
+    model.add(Dense(units=64, activation="relu"))
+    model.add(Dense(units=32, activation="relu"))
+    # Ultimately, end with dense layer with softmax
+    model.add(Dense(input_dim=embedding_dim, units=num_labels, activation="softmax"))
+    # Compile model using our settings, check for accuracy
+    model.compile(loss=loss_function, optimizer=optim, metrics=['accuracy'])
+    return model
+
+def train_model(model, X_train, Y_train, X_dev, Y_dev, args):
+    '''Train the model here. Note the different settings you can experiment with!'''
+    # Potentially change these to cmd line args again
+    # And yes, don't be afraid to experiment!
+    verbose = args.verbose
+    batch_size = args.batch_size
+    epochs = args.epochs
+    # Early stopping: stop training when there are three consecutive epochs without improving
+    # It's also possible to monitor the training loss with monitor="loss"
+    callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+    # Finally fit the model to our data
+    model.fit(X_train, Y_train, verbose=verbose, epochs=epochs, callbacks=[callback], batch_size=batch_size, validation_data=(X_dev, Y_dev))
+    # Print final accuracy for the model (clearer overview)
+    test_set_predict(model, X_dev, Y_dev, "dev", args)
+    return model
+
+def test_set_predict(model, X_test, Y_test, ident, args):
+    '''Do predictions and measure accuracy on our own test set (that we split off train)'''
+    # Get predictions using the trained model
+    Y_pred = model.predict(X_test)
+    # Finally, convert to numerical labels to get scores with sklearn
+    Y_pred = np.argmax(Y_pred, axis=1)
+    # If you have gold data, you can calculate accuracy
+    Y_test = np.argmax(Y_test, axis=1)
+    # Print classification report and confusion matrix
+    print(f'Classification Report on own {ident} set:\n{classification_report(Y_test, Y_pred)}')
+    print(f'Confusion Matrix on own {ident} set:\n{confusion_matrix(Y_test, Y_pred)}')
+
+def main():
+    '''Main function to train and test neural network given cmd line arguments'''
+    args = create_arg_parser()
+
+    # Read in the data and embeddings
+    X_train, Y_train = read_corpus(args.train_file)
+    X_dev, Y_dev = read_corpus(args.dev_file)
+    embeddings = read_embeddings(args.embeddings)
+
+    # Transform words to indices using a vectorizer
+    vectorizer = TextVectorization(standardize=None, output_sequence_length=50)
+    # Use train and dev to create vocab - could also do just train
+    text_ds = tf.data.Dataset.from_tensor_slices(X_train + X_dev)
+    vectorizer.adapt(text_ds)
+    # Dictionary mapping words to idx
+    voc = vectorizer.get_vocabulary()
+    emb_matrix = get_emb_matrix(voc, embeddings)
+
+    # Transform string labels to one-hot encodings
+    encoder = LabelBinarizer()
+    Y_train_bin = encoder.fit_transform(Y_train)  # Use encoder.classes_ to find mapping back
+    Y_dev_bin = encoder.fit_transform(Y_dev)
+
+    # Create model
+    model = create_model(Y_train, emb_matrix, args)
+
+    # Transform input to vectorized input
+    X_train_vect = vectorizer(np.array([[s] for s in X_train])).numpy()
+    X_dev_vect = vectorizer(np.array([[s] for s in X_dev])).numpy()
+
+    # Train the model
+    model = train_model(model, X_train_vect, Y_train_bin, X_dev_vect, Y_dev_bin, args)
+
+    # Do predictions on specified test set
+    if args.test_file:
+        # Read in test set and vectorize
+        X_test, Y_test = read_corpus(args.test_file)
+        Y_test_bin = encoder.fit_transform(Y_test)
+        X_test_vect = vectorizer(np.array([[s] for s in X_test])).numpy()
+        # Finally do the predictions
+        test_set_predict(model, X_test_vect, Y_test_bin, "test", args)
+
+if __name__ == '__main__':
+    main()
