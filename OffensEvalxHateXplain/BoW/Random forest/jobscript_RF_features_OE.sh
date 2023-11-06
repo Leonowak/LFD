4575ceb76f8e10c3a04cf6c5f745bd26263f4014
elenakireva <124815936+elenakireva@users.noreply.github.com>
2023-11-06 20:23:11 +0100

    Add files via upload

---
+++ OffensEvalxHateXplain/BoW/Random forest/jobscript_RF_features_OE.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=RF_features_OE_test
+#SBATCH --time=06:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --output=RF_features_OE_test.log
+#SBATCH --mem=64GB
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+
+source $HOME/venvs/my_env/bin/activate
+
+python3 --version
+which python3
+
+python bag-of-words.py -tf dev_offens.json -df test_offens.json -t --algorithm rf --n_estimators 300 --min_samples_split 2 -ngram 7 -char 1 -pos
+
+deactivate
